53367853f18df93732556b7aa0fedc148061084f
di3upham <di3upham@gmail.com>
2021-03-10 16:22:11 +0700

    read user session

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package userclient
 
 import (
 	"context"
+	"strings"
 	"sync"
 	"time"
 
@@ -102,8 +103,145 @@ func GetUser(accid, userid string) (*header.User, error) {
 		return nil, errors.Wrap(err, 500, errors.E_proto_marshal_error, accid, userid)
 	}
 
+	lastSessionId := GetTextAttr(u, "latest_session_id")
+	session, err := GetUserSession(accid, userid, lastSessionId)
+	if err != nil {
+		return nil, err
+	}
+	u.Session = session
+
 	cache.SetWithTTL(accid+userid, u, 1000, 30*time.Second)
 	return u, nil
+}
+
+// Ref git.subiz.net/user/com/common.go
+func GetTextAttr(u *header.User, key string) string {
+	key = strings.ToLower(strings.TrimSpace(key))
+	for _, a := range u.GetAttributes() {
+		if !SameKey(key, a.GetKey()) {
+			continue
+		}
+
+		return a.GetText()
+	}
+
+	return ""
+}
+
+func SameKey(k1, k2 string) bool {
+	return strings.TrimSpace(strings.ToLower(k1)) == strings.TrimSpace(strings.ToLower(k2))
+}
+
+// Ref git.subiz.net/user/session/sessiondb.go
+func GetUserSession(accountId, userId, sessionId string) (*header.UserSession, error) {
+	var ip string
+	var timezone string
+	var platform string
+	var language string
+	var userAgent string
+	var screenResolution string
+	var gaClientId string
+	var adsNetwork string
+	// srcTracebs := make([][]byte, 0)
+	var referrer string
+	var dstReferrer string
+	var source string
+	var gaTrackingIds []string
+	startEventb := make([]byte, 0)
+	latestEventb := make([]byte, 0)
+	campaignbs := make([][]byte, 0)
+	var timeOnSite int64
+
+	err := cqlsession.Query(
+		`SELECT
+			ip,
+			timezone,
+			platform,
+			language,
+			user_agent,
+			screen_resolution,
+			ga_client_id,
+			ads_network,
+			referrer,
+			destination_referrer,
+			source,
+			campaigns,
+			ga_tracking_ids,
+			start_event,
+			latest_event,
+			time_on_site
+		FROM user_session
+		WHERE account_id=? AND user_id=? AND id=?`,
+		accountId, userId, sessionId,
+	).Scan(
+		&ip,
+		&timezone,
+		&platform,
+		&language,
+		&userAgent,
+		&screenResolution,
+		&gaClientId,
+		&adsNetwork,
+		// &srcTracebs,
+		&referrer,
+		&dstReferrer,
+		&source,
+		&campaignbs,
+		&gaTrackingIds,
+		&startEventb,
+		&latestEventb,
+		&timeOnSite,
+	)
+
+	if err != nil && err.Error() == gocql.ErrNotFound.Error() {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, errors.Wrap(err, 500, errors.E_database_error, accountId, userId, sessionId)
+	}
+
+	var startEvent, latestEvent header.Event
+	proto.Unmarshal(startEventb, &startEvent)
+	proto.Unmarshal(latestEventb, &latestEvent)
+
+	campaigns := make([]*header.SessionCampaign, 0)
+	for _, campaignb := range campaignbs {
+		campaign := &header.SessionCampaign{}
+		proto.Unmarshal(campaignb, campaign)
+		campaigns = append(campaigns, campaign)
+	}
+
+	// srcTraces := make([]*header.SourceTrace, 0)
+	// for _, srcTraceb := range srcTracebs {
+	// 	srcTrace := &header.SourceTrace{}
+	// 	proto.Unmarshal(srcTraceb, srcTrace)
+	// 	srcTraces = append(srcTraces, srcTrace)
+	// }
+
+	out := &header.UserSession{
+		AccountId:        accountId,
+		UserId:           userId,
+		Id:               sessionId,
+		Ip:               ip,
+		Timezone:         timezone,
+		Platform:         platform,
+		Language:         language,
+		UserAgent:        userAgent,
+		ScreenResolution: screenResolution,
+		GaClientId:       gaClientId,
+		AdsNetwork:       adsNetwork,
+		// SourceTraces:        srcTraces,
+		Referrer:            referrer,
+		DestinationReferrer: dstReferrer,
+		Source:              source,
+		Campaigns:           campaigns,
+		GaTrackingIds:       gaTrackingIds,
+		StartEvent:          &startEvent,
+		LatestEvent:         &latestEvent,
+		TimeOnSite:          timeOnSite,
+	}
+
+	return out, nil
 }
 
 func SetUser(ctx *cpb.Context, u *header.User) error {
